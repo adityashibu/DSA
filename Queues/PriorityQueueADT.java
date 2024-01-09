3e0997bc41e82d3e87d6b2e960c1dfc73a211fce
Aditya Shibu <as2397@hw.ac.uk>
2024-01-10 01:16:06 +0400

    Completed priorityQueue

--- Queues/PriorityQueueADT.java
+++ Queues/PriorityQueueADT.java
@@ -1,4 +1,9 @@
 package Queues;
 
 public interface PriorityQueueADT {
+    public int size();
+    public boolean isEmpty();
+    public int min() throws PriorityQueueException;
+    public int removeMin() throws PriorityQueueException;
+    public void insert(int j);
 }
